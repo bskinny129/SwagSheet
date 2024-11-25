f15552bae63dc94a7798119642b1d10ec3ac7267
Brian Skinner <brian@breakoutmentors.com>
2024-11-25 15:38:15 -0800

    more table tweaks

--- src/components/csv/ColumnManager.tsx
+++ src/components/csv/ColumnManager.tsx
@@ -71,9 +71,9 @@ export function ColumnManager({
         <Table className="font-normal w-full table-fixed">
           <TableHeader>
             <TableRow className="text-md sm:text-lg bg-primary-very-light-purple/30 hover:bg-primary-very-light-purple/30 border-primary-very-dark">
-              <TableHead className="w-10 sm:w-16 py-6 font-semibold"></TableHead>
-              <TableHead className="text-gray-600 py-6 font-semibold align-top pt-7 sm:pt-6">Column Name</TableHead>
-              <TableHead className="w-24 sm:w-40 text-gray-600 py-6 font-semibold align-top">
+              <TableHead className="w-10 sm:w-16"></TableHead>
+              <TableHead className="text-gray-600 font-semibold align-top py-3 pt-4 sm:pt-6">Column Name</TableHead>
+              <TableHead className="w-20 sm:w-40 text-gray-600 font-semibold align-top py-3 sm:py-6">
                 <div className="flex flex-col sm:flex-row items-center">
                   <Checkbox
                     checked={selectAll}
